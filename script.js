5b301f1fbd677fb605b62f5b9ba89eae8fe7535d
THIAGO DE LIMA AUGUSTIM <148623853+Thiago-Augustim@users.noreply.github.com>
2025-11-13 20:15:16 -0300

    Animação recolher menu

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const botaoFecharMenu = document.getElementById('botao-fechar-menu');
+const menuLateral = document.getElementById('menu-lateral');
+const iconExibirMenu = document.querySelector('#menu-lateral div button img');
+
+
+botaoFecharMenu.addEventListener('click', () => {
+    menuLateral.classList.toggle('menu-fechado');
+    iconExibirMenu.classList.toggle('icon-abrir-menu');
+});
 
