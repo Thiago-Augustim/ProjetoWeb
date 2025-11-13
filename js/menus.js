5056933b9b2c6cf67afe09562093ddd1af57f8d5
THIAGO DE LIMA AUGUSTIM <148623853+Thiago-Augustim@users.noreply.github.com>
2025-11-13 20:57:57 -0300

    ajuste de icone e pacotes

--- js/menus.js
+++ js/menus.js
@@ -1,10 +1,12 @@
 const botaoFecharMenu = document.getElementById('botao-fechar-menu');
 const menuLateral = document.getElementById('menu-lateral');
 const iconExibirMenu = document.querySelector('#menu-lateral div button img');
+const janelaCentral = document.querySelector('.janela-central');
 
 
 botaoFecharMenu.addEventListener('click', () => {
     menuLateral.classList.toggle('menu-fechado');
     iconExibirMenu.classList.toggle('icon-abrir-menu');
+    janelaCentral.classList.toggle('janela-menu-fechado');
 });
 
